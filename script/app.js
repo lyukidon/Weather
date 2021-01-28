8ba115aa18291ecc1288c61b97b2d6a331bc3c6a
lyukidon <qgx99098@gmail.com>
2021-01-28 23:33:48 +0900

    현재 위치 날씨 보기& 검색할 수 있도록 변경

--- script/app.js
+++ script/app.js
@@ -1,16 +1,15 @@
+//현재 위치
 function loadAPI (){
-    let tempDescription = document.querySelector('.temperature-description');
-    let tempDegree = document.querySelector('.degree')
-    let location = document.querySelector('.location');
-    let locationInput = document.querySelector('.locationInput').value;
-    console.log(locationInput)
-    
+    const tempDescription = document.querySelector('.temperature-description');
+    const tempDegree = document.querySelector('.degree');
+    const location = document.querySelector('.location');
+
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position=>{
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api = `http://api.openweathermap.org/data/2.5/weather?q=${locationInput}&appid=c0eb657b1620478cc82c72581f8128ba`
+            const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=c0eb657b1620478cc82c72581f8128ba`
             fetch(api)
                 .then(response => {
                     return response.json();
@@ -30,10 +29,36 @@ function loadAPI (){
         })
     }
 }
-
+//검색한 위치
+function loadSearchAPI(){
+    const tempDegree = document.querySelector('.degree');
+    const location = document.querySelector('.location');
+    const locationInput = document.querySelector('#locationInput').value;
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&appid=c0eb657b1620478cc82c72581f8128ba`
+    fetch(api)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            //위치
+            location.innerHTML = data.name;
+            country.innerHTML = data.sys.country;
+            //아이콘!!
+            let iconcode = data.weather[0].icon;
+            let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+            document.getElementById('wicon').src = iconurl;
+            //온도
+            tempDegree.innerHTML = Math.round(data.main.temp-273.15)+' °C'
+        })
+}
 window.addEventListener('load',loadAPI);
-document.querySelector('.locationInput').addEventListener('keydown',(e)=>{
+
+document.querySelector('#locationInput').addEventListener('keydown',(e)=>{
     if (e.keyCode === 13){
-        loadAPI();
+        loadSearchAPI();
+        document.querySelector('#locationInput').value = '';
     }
-})
+});
+document.querySelector('#search').addEventListener('click', loadSearchAPI);
+document.querySelector('#currentPosition').addEventListener('click', loadAPI);
